004b6bf382e5f014c0a41a97076e6b00c47979d6
Gabriel Fournier <Gabriel.Fournier@USherbrooke.ca>
2015-05-20 16:58:07 -0400

    Fixed the build

--- routes/refunds.js
+++ routes/refunds.js
@@ -2,7 +2,7 @@ require('../models/user.js');
 var mongoose = require("mongoose");
 var User = mongoose.model('User');
 
-module.exports = function refunds (req, res) {
+module.exports.refunds = function (req, res) {
 	// TODO: pick up data from session/Mongo instead of seed
 	var user = new User({
 		cip: 'foug1803',
